c6197a33a67dab712ce14a7e3dd110db97326a62
Kir Jarchow <Kir@MyNameIsKir.com>
2017-11-19 02:56:15 -0800

    Missed another spot

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ const Discord   = require('discord.js')
 const client    = new Discord.Client()
 const modules   = require('../modules')
 
-client.on('ready', function() {
+client.on('ready', () => {
   console.log("Logged in as " + client.user.tag)
 })
 
@@ -24,13 +24,13 @@ client.on('message', message => {
 //create event listener for new members
 client.on('guildMemberAdd', member => {
     //Sends greeting to the default channel mentioning the member.
-    member.guild.defaultChannel.send(`Welcome to the server, ${member}!`)
+    member.guild.defaultChannel.send(`Welcome to the server, ${member.displayName}!`)
 })
 
 //create event listener for when members leave server
 client.on('guildMemberRemove', member => {
     //Sends leave message to the default channel 
-    member.guild.defaultChannel.send(`${member} has left the server.`)
+    member.guild.defaultChannel.send(`${member.displayName} has left the server.`)
 })
 
 module.exports = client
