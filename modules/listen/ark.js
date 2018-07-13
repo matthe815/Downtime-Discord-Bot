73a57eaf013a3fb2e3d811ad0edc4965c08f27fb
Kir Jarchow <Kir@MyNameIsKir.com>
2018-07-13 12:33:21 -0700

    Change ssq library

--- modules/listen/ark.js
+++ modules/listen/ark.js
@@ -1,31 +1,31 @@
-const ssq = require('ssq');
-const keys    = require('../../util/keys')
+const ssq     = require('source-server-query');
+const keys    = require('../../util/keys');
+const TIMEOUT = 3000;
 
 function info(channel, admin, cUser) {
-  ssq.info(...keys.ARK, (err, info) => {
-    if (err) {
-      if (err.code === 'ENOTFOUND')
-        channel.send(`Uh oh! By my calculations, there's a disturbance in the ark server. Hey ${admin.toString()}, could you check this out?`)
-      else
-        console.log("There was an error fetching ark server info: ", err)
-    } else {
-      cUser.setGame(`${info.numplayers} players in the Ark`)
-    }
-  })
+  ssq.info(...keys.ARK, TIMEOUT)
+    .then((info) => {
+      if (!info.hasOwnProperty('game')) {
+        console.log(channel.send(`Uh oh! By my calculations, there's a disturbance in the ark server. Hey ${admin.toString()}, could you check this out?`))
+      } else {
+        cUser.setGame(`${info.playersnum} players in the Ark`)
+      }
+    })
+    .catch(() => {
+      console.log("There was an error fetching ark server info: ", err)
+    })
 }
 
 function players(channel) {
-  ssq.players(...keys.ARK, (err, players) => {
-    if (err)
-      console.log("There was an error fetching ark server players: ", err)
-    else
+  ssq.players(...keys.ARK, TIMEOUT)
+    .then((players) => {
       players.forEach((player) => {
         if (player.name !== '' && player.duration > 10800) {
           const duration = Math.floor(player.duration / 3600)
           channel.send(`${player.name} has been online for over ${duration} hours. Please take a break.`)
         }
       })
-  })
+    })
 }
 
 module.exports.listen = function(client) {
