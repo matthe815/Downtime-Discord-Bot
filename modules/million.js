5bfc79ea35c8b89d7ff3d722442759d8f23d26c0
Matthe815 <matthew.elswick@matthe815.dev>
2022-10-15 03:10:10 -0400

    safeDump fix

--- modules/million.js
+++ modules/million.js
@@ -39,7 +39,7 @@ function writeScores(){
       SCORES[user].refused++
     }
   }
-  fs.writeFile(SCORESFILE, yaml.safeDump(SCORES))
+  fs.writeFile(SCORESFILE, yaml.dump(SCORES))
   roundResults = {}
 }
 
