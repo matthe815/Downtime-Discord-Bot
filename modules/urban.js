9ec3785af4ad52ca7effff680bf931415dfb6d24
Matthe815 <matthew.elswick@matthe815.dev>
2022-10-15 14:40:11 -0400

    refactor: further urban cleaning

--- modules/urban.js
+++ modules/urban.js
@@ -1,10 +1,14 @@
 const { EmbedBuilder } = require('@discordjs/builders')
 const request = require('request')
 
-module.exports.urban = function (message) {
+module.exports.urban = (message) => {
   const query = message.content.slice(6)
-  request(`http://api.urbandictionary.com/v0/define?term=${query}`, (err, res, body) => {
-    if (err) console.log(err)
+
+  // Expect something after the message
+  if (query.trim() === '') return message.channel.send('Invalid arguments, expecting something after the command.')
+
+  request(`http://api.urbandictionary.com/v0/define?term=${query}`, (err, _, body) => {
+    if (err) return console.log(err) // Safely handle errors.
 
     try {
       const result = JSON.parse(body).list[0]
@@ -12,12 +16,12 @@ module.exports.urban = function (message) {
       const embed = new EmbedBuilder()
         .setAuthor({ name: 'Urban Dictionary', iconURL: 'https://lh5.ggpht.com/oJ67p2f1o35dzQQ9fVMdGRtA7jKQdxUFSQ7vYstyqTp-Xh-H5BAN4T5_abmev3kz55GH=w300' })
         .setColor(15728384)
-        .setTitle(result.world)
+        .setTitle(result.word)
         .setDescription(result.definition)
         .setURL(result.permalink)
         .setFooter({ text: `By: ${result.author}` })
 
-      message.channel.send(embed)
+      message.channel.send({ embeds: [embed] })
     } catch (e) {
       console.log(e)
       message.channel.send('Failed to get data from urbandictionary')
