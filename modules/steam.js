b9e7ea3a960a7b0bd2aed8bfcf0eb04ed14c23c3
Matthe815 <matthew.elswick@matthe815.dev>
2022-10-15 14:32:38 -0400

    refactor: steam.js & urban.js

--- modules/steam.js
+++ modules/steam.js
@@ -2,93 +2,101 @@ const keys = require('../util/keys')
 const SteamApi = require('steam-api')
 const request = require('request')
 
-const app = new SteamApi.App(keys.STEAM.KEY)
 const player = new SteamApi.Player(keys.STEAM.KEY)
 const user = new SteamApi.User(keys.STEAM.KEY)
 
 // Displays nickname and profile link of the Steam ID (64-bit) presented
 module.exports.GetPlayerDetails = function (message) {
-  steamId = message.content.slice(9)
-  if (steamId) {
-    url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + keys.STEAM.KEY + '&steamids=' + steamId
+  const steamId = message.content.slice(9)
 
-    request(url, function (err, res, body) {
-      json = JSON.parse(body)
-      profile = json.response.players[0]
-      message.channel.send('Nickname: ' + profile.personaname + '\n' + profile.profileurl + '\n')
-    })
-  }
+  if (steamId.trim() === '') return
+
+  const url = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + keys.STEAM.KEY + '&steamids=' + steamId
+
+  request(url, (err, res, body) => {
+    if (err) console.log(err)
+
+    const json = JSON.parse(body)
+    const profile = json.response.players[0]
+
+    message.channel.send(`Nickname: ${profile.personaname}\n${profile.profileurl}`)
+  })
 }
 
 // Displays the level of the user associated to the Steam ID (64-bit)
-module.exports.GetPlayerLevel = function (message) {
-  steamId = message.content.slice(7)
-  if (steamId) {
-    player.GetSteamLevel(steamId).done(function (result) {
-      message.channel.send('Steam Level is ' + result)
-    })
-  }
+module.exports.GetPlayerLevel = (message) => {
+  const steamId = message.content.slice(7)
+
+  if (steamId.trim() !== '') return
+
+  player.GetSteamLevel(steamId).done((result) => {
+    message.channel.send(`Steam Level is: ${result}`)
+  })
 }
 
 // Sends in Direct Message all Steam friends from the associated Steam ID (64-bit)
 // in the format nickname, profile link for each friend
 module.exports.GetSteamFriends = function (message) {
-  steamId = message.content.slice(9)
-  if (steamId) {
-    user.GetFriendList(optionalRelationship = 'all', steamId).done(function (result) {
-      friends = []
-      result.forEach(function (player) {
-        friends.push('Nickname: ' + player.personaName + '\n' + player.profileUrl + '\n')
-      })
-      friendArr = splitStrByTextLimit(friends.join('\n'))
-      message.channel.send('Check your DMs')
-      message.author.sendMessage('Steam friends list: \n')
-      friendArr.forEach(function (block) {
-        message.author.sendMessage(block)
-      })
+  const steamId = message.content.slice(9)
+
+  if (steamId.trim() === '') return
+
+  user.GetFriendList('all', steamId).done((result) => {
+    const friends = []
+    result.forEach((player) => {
+      friends.push(`Nickname: ${player.personaname}\n${player.profileurl}`)
     })
-  }
+    const friendArr = splitStrByTextLimit(friends.join('\n'))
+    message.channel.send('Check your DMs')
+    message.author.sendMessage('Steam friends list: \n')
+    friendArr.forEach((block) => {
+      message.author.sendMessage(block)
+    })
+  })
 }
 
 // Feel the shame of how many games are unplayed by the associated Steam ID (64-bit)
 module.exports.GetNumUnPlayedGames = function (message) {
-  steamId = message.content.slice(11)
-  if (steamId) {
-    player.GetOwnedGames(steamId)
-      .catch(function (err) {
-        console.log(err)
+  const steamId = message.content.slice(11)
+
+  if (steamId.trim === '') return
+
+  player.GetOwnedGames(steamId)
+    .catch((err) => {
+      console.log(err)
+    })
+    .done((result) => {
+      let numUnplayed = 0
+      let total = 0
+      const listUnplayed = []
+
+      result.forEach((game) => {
+        total += 1
+        if (game.playtimeForever === '0') {
+          numUnplayed += 1
+          listUnplayed.push(game.name)
+        }
       })
-      .done(function (result) {
-        numUnplayed = 0
-        total = 0
-        listUnplayed = []
-        result.forEach(function (game) {
-          total += 1
-          if (game.playtimeForever == '0') {
-            numUnplayed += 1
-            listUnplayed.push(game.name)
-          }
-        })
-        gameArr = splitStrByTextLimit(listUnplayed.join('\n'))
-        message.channel.send('shame shame ' + numUnplayed.toString() + ' unplayed games out of ' + total.toString() + '\n' +
-                            'List of shame sent via DMs')
-        message.author.sendMessage('YOUR LIST OF SHAME:\n')
-        gameArr.forEach(function (block) {
-          message.author.sendMessage(block)
-        })
+      const gameArr = splitStrByTextLimit(listUnplayed.join('\n'))
+      message.channel.send(`shame shame ${numUnplayed.toString()} unplayed games out of ${total.toString()}\n
+                            List of shame sent via DMs`)
+
+      message.author.sendMessage('YOUR LIST OF SHAME:\n')
+      gameArr.forEach((block) => {
+        message.author.sendMessage(block)
       })
-  }
+    })
 }
 
 // Splits the string into the maximum allowed characters returned in an array
 function splitStrByTextLimit (lineOfText) {
-  maxLength = 2000 // Discord's max character limit
-  parsedList = []
+  const maxLength = 2000 // Discord's max character limit
+  const parsedList = []
   while (lineOfText.length > maxLength) {
-    pos = lineOfText.substring(0, maxLength).lastIndexOf('\n')
+    let pos = lineOfText.substring(0, maxLength).lastIndexOf('\n')
     pos = pos <= 0 ? maxLength : pos
     parsedList.push(lineOfText.substring(0, pos))
-    i = lineOfText.indexOf('\n', pos) + 1
+    let i = lineOfText.indexOf('\n', pos) + 1
     if (i < pos || i > pos + 1) {
       i = pos
     }
--- modules/urban.js
+++ modules/urban.js
@@ -1,25 +1,23 @@
+const { EmbedBuilder } = require('@discordjs/builders')
 const request = require('request')
 
 module.exports.urban = function (message) {
   const query = message.content.slice(6)
   request(`http://api.urbandictionary.com/v0/define?term=${query}`, (err, res, body) => {
+    if (err) console.log(err)
+
     try {
       const result = JSON.parse(body).list[0]
-      message.channel.send({
-        embed: {
-          author: {
-            name: 'urban dictionary',
-            icon_url: 'https://lh5.ggpht.com/oJ67p2f1o35dzQQ9fVMdGRtA7jKQdxUFSQ7vYstyqTp-Xh-H5BAN4T5_abmev3kz55GH=w300'
-          },
-          color: 15728384,
-          title: result.word,
-          url: result.permalink,
-          description: result.definition,
-          footer: {
-            text: `by ${result.author}`
-          }
-        }
-      })
+
+      const embed = new EmbedBuilder()
+        .setAuthor({ name: 'Urban Dictionary', iconURL: 'https://lh5.ggpht.com/oJ67p2f1o35dzQQ9fVMdGRtA7jKQdxUFSQ7vYstyqTp-Xh-H5BAN4T5_abmev3kz55GH=w300' })
+        .setColor(15728384)
+        .setTitle(result.world)
+        .setDescription(result.definition)
+        .setURL(result.permalink)
+        .setFooter({ text: `By: ${result.author}` })
+
+      message.channel.send(embed)
     } catch (e) {
       console.log(e)
       message.channel.send('Failed to get data from urbandictionary')
